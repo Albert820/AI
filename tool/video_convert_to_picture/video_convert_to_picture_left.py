e5b1eae034a7efe7fa044419563726b4fe7c3162
albert <kevin1993528@gmail.com>
2022-08-22 18:11:52 +0800

    Revert "add lab tool"
    This reverts commit 8ad3addea6468c76ed76ccd1f65bff4798735280.

--- tool/video_convert_to_picture/video_convert_to_picture_left.py
+++
@@ -1,9 +0,0 @@
-import cv2
-vidcap = cv2.VideoCapture('/home/cheng/dir_for_label_data/龍佃/0407/gopro/0408/左/GH031772.MP4')
-success,image = vidcap.read()
-count = 0
-while success:
-  cv2.imwrite("/home/cheng/dir_for_label_data/left_cut_pic/%d.jpg" % count, image)     # save frame as JPEG file      
-  success,image = vidcap.read()
-  print('Read a new frame: ', success)
-  count += 1
--- tool/video_convert_to_picture/video_convert_to_picture_right.py
+++
@@ -1,9 +0,0 @@
-import cv2
-vidcap = cv2.VideoCapture('/home/cheng/dir_for_label_data/龍佃/0407/gopro/0408/右/GH031776.MP4')
-success,image = vidcap.read()
-count = 0
-while success:
-  cv2.imwrite("/home/cheng/dir_for_label_data/right_cut_pic/%d.jpg" % count, image)     # save frame as JPEG file      
-  success,image = vidcap.read()
-  print('Read a new frame: ', success)
-  count += 1
